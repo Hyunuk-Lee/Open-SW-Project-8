8190edbf63142c37ab03642918ce7411f0e98d61
Hyunuk-Lee <air244302@gmail.com>
2026-03-20 15:04:26 +0900

    내부 git 삭제 및 전체 파일 추가

---
+++ react-test/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          공개 sw 프로젝트 8조 우하하
+        </p>
+        <p>
+          이현욱 정종욱 구민주 설소연
+        </p>
+        <p>
+          ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
